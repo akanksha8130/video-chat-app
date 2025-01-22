0f54d1898cf970036c06d7740e2a0bc722df3ab6
akanksha8130 <71652318+akanksha8130@users.noreply.github.com>
2025-01-22 17:55:04 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -7,25 +7,26 @@ var peer = new Peer(undefined, {
 });
 
 const user = prompt("Enter your name");
-const myVideo=document.createElement("video")
+const myVideo=document.createElement('video')
 myVideo.muted=true
-
 let myStream
 navigator.mediaDevices
 .getUserMedia({
-    video:true
+    audio:true,
+    video:true,
 })
 .then((stream)=>{
     myStream=stream
+    addvideo(myVideo,stream)
 })
-function addVideoStream(video,stream){
-video.srcObject=stream
-video.addEventListener("loadedmetadata",()=>{
-    video.play()
-$("#video_grid").append(video)
-})
-}
 
+function addvideo(video,stream){
+    video.srcObject=stream
+    video.addEventListener('loadedmetadata',()=>{
+        video.play()
+        $("#video_grid").append(video)
+    })
+}
 $(function () {
     $("#show_chat").click(function () {
         $(".left-window").css("display", "none")
