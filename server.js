664bb0d41764dc793cf555f8c7454f99859740ed
akanksha8130 <71652318+akanksha8130@users.noreply.github.com>
2025-02-26 18:19:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 const { v4: uuidv4 } = require("uuid");
+const nodemailer=require("nodemailer")
 
+const transporter=nodemailer.createTransport({
+    port:587,
+    host:"smtp.gmail.com",
+    auth:{
+        user:"akanksha20shukla@gmail.com",
+        pass:'rhkjrisqaipymbfe'
+    },
+    secure:true
+})
 const io = require("socket.io")(server, {
     cors: {
         origin: '*'
@@ -16,6 +26,25 @@ const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
     debug: true,
 });
+
+app.post('/send-mail',(req,res)=>{
+    const to=req.body.to 
+    const url=req.body.url
+    const maildata={
+        from:'akanksha20shukla@gmail.com',
+        to:to,
+        subject:'join the video chat',
+        html:`<p>Hey there,</p><p>Come and join me for a class here- ${url}</p>`
+    }
+    transporter.sendMail(maildata,(error,info)=>{
+        if(error){
+            return console.log(error)
+        }
+        else{
+        res.status(200).send({message:"InvitationSent!",message_id:info.messageId})
+        }
+    })
+})
 
 app.use("/peerjs", peerServer);
 
